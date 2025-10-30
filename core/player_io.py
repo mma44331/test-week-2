39e05115b871d04474dff6ecb187003990fce1c4
meyir moshe <mma088651259@gmail.com>
2025-10-30 12:51:49 +0200

    test week 2

--- core/player_io.py
+++ core/player_io.py
@@ -1,6 +1,6 @@
 def ask_player_action() -> str:
     while True:
         char = input("Entera character only 'S' or 'H': ")
-        if char != "S" or char != "H":
-            continue
+        if char == "S" or char == "H":
+            break
     return char
